20bbd8c6685db197731b89396764166a9728db99
Debanshu2005 <debanshusarkar187@gmail.com>
2026-08-28 10:46:38 +0530

    cleanup the repo

--- onboard_edge/test_parser2.py
+++
@@ -1,4 +0,0 @@
-import sys
-from trajectory_engine import parse_mission
-print("takeoff:", parse_mission("takeoff at an height of 5m."))
-print("arm:", parse_mission("arm"))
--- onboard_edge/test_parser3.py
+++
@@ -1,3 +0,0 @@
-import sys
-from trajectory_engine import _normalize_text
-print("norm:", _normalize_text("takeoff at an height of 5m."))
